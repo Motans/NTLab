4cfeb6069f9361908a7f57b57a80e6babbd79391
Motans <romamakovskii@gmail.com>
2021-03-10 17:53:32 +0300

    oversampling to dc_remove

--- source/dc_remove.vhd
+++ source/dc_remove.vhd
@@ -20,22 +20,22 @@ end dc_remove;
 architecture dc_remove_arch of dc_remove is
   begin
     process(clk, Rs)
-        variable buf        : signed(dig_size-1 downto 0) := to_signed(0, dig_size);
-        variable x_i        : signed(dig_size-1 downto 0) := to_signed(0, dig_size);
-        variable y_i        : signed(dig_size-1 downto 0) := to_signed(0, dig_size);
-        variable mult_res   : signed(2*dig_size-1 downto 0) := to_signed(0, 2*dig_size);
+        variable buf        : signed(2*dig_size-1 downto 0);-- := to_signed(0, 2*dig_size);
+        variable x_i        : signed(2*dig_size-1 downto 0);-- := to_signed(0, 2*dig_size);
+        variable y_i        : signed(2*dig_size-1 downto 0);-- := to_signed(0, 2*dig_size);
+        variable mult_res   : signed(3*dig_size-1 downto 0);-- := to_signed(0, 3*dig_size);
       begin
         if (Rs = '1') then
-            buf := to_signed(0, dig_size);
-            x_i := to_signed(0, dig_size);
+            buf := to_signed(0, 2*dig_size);
+            x_i := to_signed(0, 2*dig_size);
         elsif (rising_edge(clk)) then
-            x_i := signed(filter_in);
+            x_i := shift_left(resize(signed(filter_in), 2*dig_size), dig_size);
 
             y_i := x_i + buf;
             mult_res := shift_left(signed(alpha)*y_i, 1);
-            buf := mult_res(2*dig_size - 1 downto dig_size) - x_i;
+            buf := mult_res(3*dig_size - 1 downto dig_size) - x_i;
 
-            filter_out <= std_logic_vector(y_i);
+            filter_out <= std_logic_vector(y_i(2*dig_size - 1 downto dig_size));
         end if;
     end process;
 end dc_remove_arch;
